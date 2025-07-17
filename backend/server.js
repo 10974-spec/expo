e802cfe5d3e95a33389a233f716eb0e6e58391f1
10974-spec <emmanuelnyakoe13@gmail.com>
2025-07-17 14:42:49 +0300

    initializing the transactions table

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 
 import express from "express";
 import dotenv from "dotenv";
+import { sql } from "./config/db";
 
 dotenv.config();
 
@@ -9,11 +10,24 @@ const app = express()
 
 const PORT = process.env.PORT || 5001;
 
+async function  initDB() {
+    try{
+        await sql`CREATE TABLE IF NOT EXISTS transactions(
+            id SERIAL PRIMARY KEY,
+            user_id VARCHAR(255) NOT NULL,
+            title VARCHAR(255) NOT NULL,
+            amount DECIMAL(10,2) NOT NULL,
+            category VARCHAR(255) NOT NULL,
+            created_at DATE NOT NULL DEFAULT CURRENT_DATE
+        )`
+    }catch(error){
+
+    }
+}
+
 app.get("/", (req, res) => {
     res.send("hello world")
 });
-
-console.log("my-port:",process.env.PORT);
 
 app.listen(PORT, () => {
     console.log("server is up and running on PORT",PORT);
