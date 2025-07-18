9bd72bc1782d3e84f70f9b7dc597803f20cbd368
10974-spec <emmanuelnyakoe13@gmail.com>
2025-07-18 12:49:16 +0300

    backend complete

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@
 
 import express from "express";
 import dotenv from "dotenv";
-import { sql } from "./config/db.js";
+import { initDB} from "./config/db.js";
 import ratelimiter from "./middleware/rateLimiter.js";
 import transactionsRoute from "./routes/transactionsRoute.js";
 
@@ -16,23 +16,6 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5001;
 
-async function  initDB() {
-    try{
-        await sql`CREATE TABLE IF NOT EXISTS transactions(
-            id SERIAL PRIMARY KEY,
-            user_id VARCHAR(255) NOT NULL,
-            title VARCHAR(255) NOT NULL,
-            amount DECIMAL(10,2) NOT NULL,
-            category VARCHAR(255) NOT NULL,
-            created_at DATE NOT NULL DEFAULT CURRENT_DATE
-        )`
-
-        console.log("Database initialized successfully")
-    }catch(error){
-        console.log("Error initializing into the DB", error);
-        process.exit(1);
-    }
-}
 
 app.use("/api/transactions",transactionsRoute)
 
