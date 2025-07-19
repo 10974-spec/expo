44481aa653e05ad4b5ffe7496dd149ce2e569fd3
10974-spec <emmanuelnyakoe13@gmail.com>
2025-07-19 19:01:16 +0300

    react native basics

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -9,7 +10,10 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={{ color: "red"}}>My very first react native application</Text>
+      <Link href="/about">
+        <Text>About</Text>
+      </Link>
     </View>
   );
 }
