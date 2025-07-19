66be746279bf1fce5d0ebb219de1686f74591c8a
10974-spec <emmanuelnyakoe13@gmail.com>
2025-07-19 20:50:19 +0300

    safe screen

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -3,17 +3,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text style={{ color: "red"}}>My very first react native application</Text>
-      <Link href="/about">
-        <Text>About</Text>
-      </Link>
+    <View>
+      <Text>My very first react native application</Text>
     </View>
   );
 }
