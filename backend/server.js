674ec53a15e90b8818c39ec04ebfb4e39243b793
10974-spec <emmanuelnyakoe13@gmail.com>
2025-07-25 17:48:42 +0300

    cron jobs

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,14 @@ import dotenv from "dotenv";
 import { initDB} from "./config/db.js";
 import ratelimiter from "./middleware/rateLimiter.js";
 import transactionsRoute from "./routes/transactionsRoute.js";
+import job from "./config/cron.js";
 
 dotenv.config();
 
 const app = express()
+
+if(process.env.NODE_ENV === "production") job.start();
+
 
 
 app.use(ratelimiter);
@@ -18,6 +22,10 @@ const PORT = process.env.PORT || 5001;
 
 
 app.use("/api/transactions",transactionsRoute)
+
+app.get("/api/health", (req, res) => {
+    res.status(200).json({ message: "OK" });
+});
 
 initDB().then(() => {
     app.listen(PORT, () => {
